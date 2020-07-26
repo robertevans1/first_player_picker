5ff643bb29f9bc1297f9f74d3eac00ab9a54a364
Rob Evans <robert.evans@ultraleap.com>
2020-07-26 17:33:37 +0100

    buttons visibility

--- lib/main.dart
+++ lib/main.dart
@@ -6,22 +6,47 @@ void main() {
 }
 
 class ExampleWidget extends StatefulWidget {
+  final numSeats = 10;
+
   @override
   ExampleWidgetState createState() => ExampleWidgetState();
 }
 
-
-class CircleButton extends StatelessWidget {
-  final GestureTapCallback onTap;
+class CircleButton extends StatefulWidget {
+  final Function(bool) onTapCallback;
   final IconData iconData;
-  final Color iconColor;
+  final Color initialColor;
 
-  const CircleButton({Key key, this.onTap, this.iconData, this.iconColor}) : super(key: key);
+  CircleButton({this.onTapCallback, this.iconData, this.initialColor});
+
+  @override
+  CircleButtonState createState() => CircleButtonState();
+}
+
+
+class CircleButtonState extends State<CircleButton> {
+  Color _iconColor;
+  GestureTapCallback onTap;
+
+    @override void initState() {
+    super.initState();
+    _iconColor = widget.initialColor;
+    onTap = () {
+      setState(() {
+        if(_iconColor == Colors.black) {
+          _iconColor = Colors.red;
+          widget.onTapCallback(true);
+        } else {
+          _iconColor = Colors.black;
+          widget.onTapCallback(false);
+        }
+      });
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
     double size = 45.0;
-
     return new InkResponse(
       onTap: onTap,
       child: new Container(
@@ -32,8 +57,34 @@ class CircleButton extends StatelessWidget {
           shape: BoxShape.circle,
         ),
         child: new Icon(
-          iconData,
-          color: iconColor,
+          widget.iconData,
+          color:_iconColor,
+        ),
+      ),
+    );
+  }
+}
+
+class SpinnerButton extends StatelessWidget {
+  final GestureTapCallback onTap;
+
+  SpinnerButton({this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    double size = 45.0;
+    return new InkResponse(
+      onTap: onTap,
+      child: new Container(
+        width: size,
+        height: size,
+        decoration: new BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+        ),
+        child: new Icon(
+          Icons.arrow_upward,
+          color: Colors.blue,
         ),
       ),
     );
@@ -42,6 +93,8 @@ class CircleButton extends StatelessWidget {
 
 class ExampleWidgetState extends State<ExampleWidget> with SingleTickerProviderStateMixin {
   AnimationController _controller;
+  List<bool> seatButtonsSelected;
+  List<bool> visibilities;
 
   @override
   void initState() {
@@ -49,6 +102,14 @@ class ExampleWidgetState extends State<ExampleWidget> with SingleTickerProviderS
       duration: const Duration(milliseconds: 1500),
       vsync: this,
     );
+
+    seatButtonsSelected = List<bool>();
+    visibilities = List<bool>();
+    for ( var i = 0 ; i < widget.numSeats; i++ ) {
+      seatButtonsSelected.add(false);
+      visibilities.add(true);
+    }
+
     super.initState();
   }
 
@@ -59,6 +120,11 @@ class ExampleWidgetState extends State<ExampleWidget> with SingleTickerProviderS
   }
 
   void spin() {
+
+    setState(() {
+      visibilities = seatButtonsSelected;
+    });
+
     _controller.reset();
     _controller.forward();
   }
@@ -73,7 +139,6 @@ class ExampleWidgetState extends State<ExampleWidget> with SingleTickerProviderS
         shape: BoxShape.circle,
       ),
     );
-    var numSeats = 10;
     return new Material(
       color: Colors.black,
       child: new Center(
@@ -83,23 +148,25 @@ class ExampleWidgetState extends State<ExampleWidget> with SingleTickerProviderS
             new Positioned(
               child: RotationTransition(
                 turns: Tween(begin: 0.0, end: 2.0).animate(_controller),
-                child: new CircleButton(onTap: () => spin(), iconColor: Colors.blue, iconData: Icons.arrow_upward,),
+                child: new SpinnerButton(onTap: () => spin()),
               ),
               top: 120.0,
               left: 130.0,
             ),
-            for ( var i = 0 ; i < numSeats; i++ )
-              Positioned(
-                child: new CircleButton(
-                  onTap: () {
-
-                  },
-                  iconData: Icons.golf_course,
-                  iconColor: Colors.black,
+            for ( var i = 0 ; i < widget.numSeats; i++ )
+              Positioned (
+                child: Visibility (
+                  visible: visibilities[i],
+                  child: new CircleButton (
+                  initialColor: Colors.black,
+                    iconData: Icons.golf_course,
+                    onTapCallback: (bool selected){ seatButtonsSelected[i] = selected;},
+                  ),
                 ),
-                top: sin(2.0 * pi * i.toDouble()/numSeats.toDouble()) * 118 + 125,
-                left: cos(2.0 * pi * i.toDouble()/numSeats.toDouble()) * 118 + 125,
-              ),
+                top: sin(2.0 * pi * i.toDouble()/widget.numSeats.toDouble()) * 118 + 125,
+                left: cos(2.0 * pi * i.toDouble()/widget.numSeats.toDouble()) * 118 + 125,
+              )
+
           ],
         ),
       ),
